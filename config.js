0f0abdcb5f3ff0867028188194e3c1cd542369e1
Hoangcodezaq <101127326+Hoangcodezaq@users.noreply.github.com>
2022-03-08 18:23:05 +0700

    Add files via upload
    abc

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Quốc Tế Phụ Nữ';
+const giftUrl = '';
+const eventName = 'Chúc Mừng ngày 8/3';
+const titleCard = 'Thiệp gửi cô Thanh';
+const contentCard = 'nhân ngày 8/3 con chúc cô luôn trẻ đẹp, hạnh phúc bên gia đình và kiếm ra được nhiều tiền để dẫn chúng con đi chơi:))';
+
+
+const giftImage = 'cothanh2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
